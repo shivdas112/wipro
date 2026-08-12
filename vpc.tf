cddce5f45b1731c1a18658d8db373d1691e772d8
shivdas112 <shivdaswadikar2@gmail.com>
2026-08-12 16:50:56 +0530

    dddddd

--- vpc.tf
+++ vpc.tf
@@ -2,8 +2,7 @@ resource "aws_vpc" "name" {
     cidr_block = "10.0.0.0/16"
     tags = {
       env = dev-env
-      description = "this is my vpc"
-      description = "this is my vpcccc"
+      department = "cse"
     }
   
 }
